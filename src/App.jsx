5b8cad916bcbc391492ce8b9a36ddc7830ff38ea
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-04-01 11:53:03 +0700

    fix : show nothing and add limited for increment and decrement

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,19 @@ import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
+
   const increment = () => {
-    setCount(count + 1)
+    if (count < 10) setCount(count + 1)
   }
   const decrement = () => {
-    setCount(count - 1)
+    if (count > 0) setCount(count - 1)
   }
-
 
   return (
     <>
-    <Button name="Tambah" onClick={increment} />
-    <p className="mt-4 text-lg font-medium">You clicked {count} times</p>
-    <Button name="Kurang" onClick={decrement} />
-
+      <Button name="Tambah" onClick={increment} />
+      <p className="mt-4 text-lg font-medium">You clicked {count} times</p>
+      <Button name="Kurang" onClick={decrement} />
     </>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
-export default function Button(name, onClick) {
+export default function Button({ name, onClick }) {
+
   return (
     <button className="px-6 py-2 bg-emerald-500 hover:bg-emerald-600 text-white font-semibold rounded-lg transition duration-200 shadow-md active:scale-95" type="button" onClick={onClick}>
       {name}
