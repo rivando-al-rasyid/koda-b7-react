0e71abeb3616f72f49481f60320cc7a5a3d2bfe4
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-04-02 16:41:47 +0700

    fix : not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ReviewForm from "./components/ReviewForm";
+import ReviewList from "./components/ReviewList";
 
 function App() {
     const [reviews, setReviews] = useState([]);
