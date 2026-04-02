b838db0fa01a4c05112ae117f8f5bfe0e2170c4b
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-04-02 16:40:46 +0700

    cleanup : remove un used code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import ReviewForm from "./components/ReviewForm";
-import ReviewList from "./components/ReviewList";
 
 function App() {
     const [reviews, setReviews] = useState([]);
